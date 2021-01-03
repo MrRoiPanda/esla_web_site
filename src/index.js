c6917c62c76cd1c94067949e42de730f7548ae16
MrRoiPanda <61798547+MrRoiPanda@users.noreply.github.com>
2021-01-03 16:55:22 +0100

    nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log("hello");
+// menu berguer
+var menu_overlay = new Boolean("false")
+console.log(menu_overlay);
+document.getElementById("Nav-overlay").style = 'opacity: 0; display: none;';
+
+//detecter ke clique
+document.getElementById("spy-me").onmousedown = function () {
+  menu_overlay = !menu_overlay;
+
+  if (menu_overlay == false) { // on vois
+    document.getElementById("Nav-overlay").style = 'opacity: 1; display: block;';
+  }
+  if (menu_overlay == true) { // on vois pas
+    document.getElementById("Nav-overlay").style = 'opacity: 0; display: none;';
+  }
+};
