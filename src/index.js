ee48cc084eddb9a2da4d8171a1199d830751e777
MrRoiPanda <61798547+MrRoiPanda@users.noreply.github.com>
2021-01-04 22:56:12 +0100

    corect scss

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ console.log("hello");
 // menu berguer
 var menu_overlay = new Boolean("false")
 console.log(menu_overlay);
-document.getElementById("Nav-overlay").style = 'opacity: 0; display: none;';
 
-//detecter ke clique
-document.getElementById("spy-me").onmousedown = function () {
+var overlay = document.getElementById("Nav-overlay");
+var berguer = document.getElementById("nav-icon");
+var content = document.getElementsByClassName("fade-page-in");
+//var links = document.getElementsByClassName("nav-menu-link");
+
+//detecter le clique
+berguer.onclick  = function () {
   menu_overlay = !menu_overlay;
 
   if (menu_overlay == false) { // on vois
-    document.getElementById("Nav-overlay").style = 'opacity: 1; display: block;';
+    overlay.style = 'opacity: 1; display: block;';
+    berguer.classList.add("open");
+    content.style = 'opacity: 0;';
   }
   if (menu_overlay == true) { // on vois pas
-    document.getElementById("Nav-overlay").style = 'opacity: 0; display: none;';
+    content.style = 'opacity: 1;';
+    berguer.classList.remove("open");
+    overlay.style = 'opacity: 0; display: none;';
   }
 };
